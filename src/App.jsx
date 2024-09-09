de4624c1446a39de10db4eee7b60c0d16fdd833c
Gisella Ortiz de la Tabla <gisellanalia@gmail.com>
2024-09-09 14:21:00 +0200

    feat: add delete function and modal confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
+import { Routes, Route } from "react-router";
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
+import DeleteModal from "./components/DeleteModal";
 
-
-//Design
+// Design
 import { Container, CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "@mui/material/styles";
-import '@fontsource/titillium-web';
-
+import "@fontsource/titillium-web";
 
 function App() {
-	//Font
 	const theme = createTheme({
 		typography: {
 			fontFamily: "Titillium Web, sans-serif",
 		},
-	}); 
+	});
 
 	return (
-	 	<ThemeProvider  theme={theme} >
-			<CssBaseline /> <Navbar />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Navbar />
 			<Container
 				maxWidth="sm"
 				sx={{ display: "flex", justifyContent: "center" }}
 			>
 				<TaskList />
+				<Routes>
+					<Route path="/deletetask" element={<DeleteModal />} />
+				</Routes>
 			</Container>
-		</ThemeProvider> 
+		</ThemeProvider>
 	);
 }
 
---
+++ src/components/DeleteModal.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import { Modal, Fade, Button, Typography, Box } from "@mui/material";
+
+const DeleteModal = ({ open, onClose, onDelete }) => {
+	return (
+		<Modal
+			open={open}
+			onClose={onClose}
+			aria-labelledby="delete-modal-title"
+			aria-describedby="delete-modal-description"
+			closeAfterTransition
+		>
+			<Fade in={open}>
+				<Box
+					sx={{
+						position: "absolute",
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						bgcolor: "background.paper",
+						boxShadow: 24,
+						p: 4,
+						maxWidth: 400,
+						minWidth: 300,
+						textAlign: "center",
+					}}
+				>
+					<Typography id="delete-modal-title" variant="h6" gutterBottom>
+						Are you sure you want to delete?
+					</Typography>
+
+					<Button
+						onClick={onDelete}
+						variant="contained"
+						color="error"
+						component={Link}
+						to="/task"
+						sx={{ mr: 2 }}
+					>
+						Delete
+					</Button>
+					<Button
+						onClick={onClose}
+						variant="outlined"
+						component={Link}
+						to="/task"
+					>
+						Cancel
+					</Button>
+				</Box>
+			</Fade>
+		</Modal>
+	);
+};
+
+export default DeleteModal;
